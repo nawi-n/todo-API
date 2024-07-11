1b19f8ff1647457b0d1037b282900e05b1abf17c
NAWIN R <nawincpn@gmail.com>
2024-07-11 16:26:37 +0530

    added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import os
 import uvicorn
 
-if __name__ == "__main__" :
-    uvicorn.run("app.main:app",reload=True)
-
+if __name__ == "__main__":
+    host = os.getenv("HOST", "0.0.0.0")
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("app.main:app", host=host, port=port, reload=True)
